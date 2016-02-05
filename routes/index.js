ad9f9dc6e53a06c0162593ecf51ac158643f81fd
Corey <corey.hadden>
2016-02-05 11:02:38 -0600

    endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
+var key = 'MOaAChuu4FnnDUPCwQYWBq6w3SynoRGj';
+var productsAPI = 'http://stage-api.target.com/products/v3/50565775';
 /* GET home page. */
-router.get('/api/find/cartwheel/products', function(req, res, next) {
-  res.json({"Start":"here"});
+router.get('/api/find/cartwheel/products/', function(req, res, next) {
+  //res.json({"Start":"here"});
+  var keyword = req.query.keyword;
+    if(keyword){//a keyword was set.
+        makeRequest('https://stage-api.target.com/v2/products/search?searchTerm='+keyword+'&key='+key,function(body){
+            var payloadJson = eval('('+body+')');
+            res.json(payloadJson);
+        });
+       // https://api.target.com/v2/products/search?searchTerm=patio&key=[APIKey]
+    }
+  makeRequest(productsAPI+'?id_type=tcin&key='+key,function(body){
+    var payloadJson = eval('('+body+')');
+    res.json(payloadJson);
+  });
 });
 
+
+function makeRequest(url,callback){
+  request(url, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+          callback && callback(body) || console.log(body); // Print the google web page.
+       }
+  //?id_type=dpci&store_id=530&mode=online&fields=ids&key=
+  })
+}
 module.exports = router;
