91940b74c78569a4a226a673e679d1f5d148dca5
Josh.M.Dingman <Josh.M.Dingman@target.com>
2016-02-05 10:18:28 -0600

    Commit

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/api/find/cartwheel/products', function(req, res, next) {
+  res.json({"Start":"here"});
 });
 
 module.exports = router;
